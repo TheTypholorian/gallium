f6a187349d0fd7f45947a2ca2c76437d5a75f71c
evan <evanpk2020@gmail.com>
2025-02-01 09:33:33 -0700

    publishing to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,26 @@
+plugins {
+    id("maven-publish")
+}
+
 group = "net.typho"
 version = "1.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/TheTypholorian/gallium")
+
+            credentials {
+                username = project.findProperty("gpr.user").toString()
+                password = project.findProperty("gpr.token").toString()
+            }
+        }
+    }
+}
