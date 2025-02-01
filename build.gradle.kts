c362295d493be772f197e15be0e8459ec824b49f
evan <evanpk2020@gmail.com>
2025-02-01 10:21:13 -0700

    jitpack 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,25 @@
+plugins {
+    id("maven-publish")
+}
+
 group = "net.typho"
-version = "0.1"
+version = "v0.1"
 description = "The Java library for reflection-based debugging tools"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "com.github.TheTypholorian"
+            artifactId = "gallium"
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
